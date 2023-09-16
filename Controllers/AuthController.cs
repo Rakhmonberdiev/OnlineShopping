c5228efd9fb59fa4dddced7712ad5f5ab4f100ae
Jamoliddin <j.m.raxmonberdiyev@gmail.com>
2023-09-16 17:43:00 +0500

    Auth Controller

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using OnlineShopping.Data;
+using OnlineShopping.Models;
+
+namespace OnlineShopping.Controllers
+{
+    [Route("api/auth")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly AppDbContext db;
+        private ApiResponse response;
+        private string secretKey;
+        private readonly UserManager<ApplicationUser> userManager;
+        private readonly RoleManager<IdentityRole> roleManager;
+        public AuthController(AppDbContext db, IConfiguration configuration, RoleManager<IdentityRole> roleManager, UserManager<ApplicationUser> userManager)
+        {
+            this.db = db;
+            secretKey = configuration.GetValue<string>("ApiSettings:Secret");
+            response = new ApiResponse();
+            this.userManager = userManager;
+            this.roleManager = roleManager;
+        }
+    }
+}
