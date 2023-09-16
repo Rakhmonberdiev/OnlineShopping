5bafe2aad071f9ad1e31d6bdd72d55a961acc5c5
Jamoliddin <j.m.raxmonberdiyev@gmail.com>
2023-09-16 16:06:14 +0500

    Create product

--- Controllers/ProductController.cs
+++ Controllers/ProductController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using OnlineShopping.Data;
+using OnlineShopping.Dtos.ProductDtos;
 using OnlineShopping.Models;
 using System.Net;
 
@@ -25,7 +26,7 @@ namespace OnlineShopping.Controllers
             return Ok(response);
         }
 
-        [HttpGet("{id:int}")]
+        [HttpGet("{id:int}", Name = "GetProductById")]
         public async Task<IActionResult> GetProductById(int id)
         {
             if(id == 0)
@@ -42,6 +43,45 @@ namespace OnlineShopping.Controllers
             response.Result = product;
             response.StatusCode = HttpStatusCode.OK;
             return Ok(response);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<ApiResponse>> CreateProduct([FromForm] ProductCreateDto productDto)
+        {
+            try
+            {
+                if(ModelState.IsValid)
+                {
+                    Product product = new()
+                    {
+                        Name = productDto.Name,
+                        Price = productDto.Price,
+                        Category = productDto.Category,
+                        SpecialTag = productDto.SpecialTag,
+                        Description = productDto.Description,
+                        Image = productDto.Image,
+                    };
+                    db.Products.Add(product);
+                    db.SaveChanges();
+                    response.Result = product;
+                    response.StatusCode = HttpStatusCode.Created;
+                    return CreatedAtRoute("GetProductById", new { id = product.Id }, response);
+                     
+
+                }
+                else
+                {
+                    response.IsSuccess = false;
+                }
+            }
+            catch (Exception ex)
+            {
+                response.IsSuccess = false;
+                response.ErrorMessages = new List<string>() { ex.ToString() };
+
+            }
+
+            return response;
         }
     }
 }
---
+++ Dtos/ProductDtos/ProductUpdateDto.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OnlineShopping.Dtos.ProductDtos
+{
+    public class ProductUpdateDto
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Category { get; set; }
+        public string SpecialTag { get; set; }
+        [Range(1, int.MaxValue)]
+        public double Price { get; set; }
+        public string Image { get; set; }
+    }
+}
