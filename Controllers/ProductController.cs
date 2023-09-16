2596246deba4b42ada70c3deaf651039482ee5b0
Jamoliddin <j.m.raxmonberdiyev@gmail.com>
2023-09-16 14:49:17 +0500

    Get Product and API Response

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using OnlineShopping.Data;
+using OnlineShopping.Models;
+using System.Net;
+
+namespace OnlineShopping.Controllers
+{
+    [Route("api/product")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly AppDbContext db;
+        private ApiResponse response;
+        public ProductController(AppDbContext db)
+        {
+            this.db = db;
+            response = new ApiResponse();
+        }
+        [HttpGet]
+        public async Task<ActionResult<ApiResponse>> GetAllProducts()
+        {
+            response.Result = db.Products;
+            response.StatusCode = HttpStatusCode.OK;
+            return Ok(response);
+        }
+    }
+}
