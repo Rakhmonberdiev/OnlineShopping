67966b3a8f05f0532e593ab42a042b5e2f1baad5
Jamoliddin <j.m.raxmonberdiyev@gmail.com>
2023-09-17 13:51:13 +0500

    Add Security to Swagger

---
+++ Controllers/AuthTestController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace OnlineShopping.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthTestController : ControllerBase
+    {
+        [HttpGet]
+        [Authorize]
+        public async Task<ActionResult<string>> GetSomething()
+        {
+            return "Вы прошли аутентификацию";
+        }
+
+    }
+}
