8cd20a0c59faa2816cbfc76373e2a40ed1b4cfb9
Jamoliddin <j.m.raxmonberdiyev@gmail.com>
2023-09-17 15:30:59 +0500

    Order Header and Details Model

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -14,6 +14,8 @@ namespace OnlineShopping.Data
         public DbSet<Product> Products { get; set; }
         public DbSet<ShoppingCart> ShoppingCarts { get; set; }
         public DbSet<CartItem> CartItems { get; set; }
+        public DbSet<OrderHeader> OrderHeaders { get; set; }
+        public DbSet<OrderDetails> OrderDetails { get; set; }
         protected override void OnModelCreating(ModelBuilder modelbuilder)
         {
             base.OnModelCreating(modelbuilder);
