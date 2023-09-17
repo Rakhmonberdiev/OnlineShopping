b320795bdc13d5f1ce76a53a898753a82dabccfa
Jamoliddin <j.m.raxmonberdiyev@gmail.com>
2023-09-17 14:33:55 +0500

    Shopping Cart Controller and Logic

---
+++ Controllers/ShoppingCartController.cs
@@ -0,0 +1,91 @@
+﻿using Azure;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using OnlineShopping.Data;
+using OnlineShopping.Models;
+using System.Net;
+
+namespace OnlineShopping.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ShoppingCartController : ControllerBase
+    {
+        protected ApiResponse response;
+        private readonly AppDbContext _db;
+        public ShoppingCartController(AppDbContext db)
+        {
+            _db = db;
+            response = new ();
+        }
+
+
+
+        [HttpPost]
+        public async Task<ActionResult<ApiResponse>> AddOrUpdateItemInCart(string userId, int productId, int updateQuantityBy)
+        {
+            ShoppingCart shoppingCart = _db.ShoppingCarts.Include(u => u.CartItems).FirstOrDefault(u => u.UserId == userId);
+            Product product = _db.Products.FirstOrDefault(u => u.Id == productId);
+            if (product == null)
+            {
+                response.StatusCode = HttpStatusCode.BadRequest;
+                response.IsSuccess = false;
+                return BadRequest(response);
+            }
+            if (shoppingCart == null && updateQuantityBy > 0)
+            {
+
+                ShoppingCart newCart = new() { UserId = userId };
+                await _db.ShoppingCarts.AddAsync(newCart);
+                await _db.SaveChangesAsync();
+
+                CartItem newCartItem = new()
+                {
+                    ProductId = productId,
+                    Quantity = updateQuantityBy,
+                    ShoppingCartId = newCart.Id,
+                    Product = null
+                };
+                await _db.CartItems.AddAsync(newCartItem);
+                await _db.SaveChangesAsync();
+            }
+            else
+            {
+                CartItem cartItemInCart = shoppingCart.CartItems.FirstOrDefault(u => u.ProductId == productId);
+                if (cartItemInCart == null)
+                {
+                    CartItem newCartItem = new()
+                    {
+                        ProductId = productId,
+                        Quantity = updateQuantityBy,
+                        ShoppingCartId = shoppingCart.Id,
+                        Product = null
+                    };
+                    await _db.CartItems.AddAsync(newCartItem);
+                    await _db.SaveChangesAsync();
+                }
+                else
+                {
+                    int newQuantity = cartItemInCart.Quantity + updateQuantityBy;
+                    if (updateQuantityBy == 0 || newQuantity <= 0)
+                    {
+                        _db.CartItems.Remove(cartItemInCart);
+                        if (shoppingCart.CartItems.Count() == 1)
+                        {
+                            _db.ShoppingCarts.Remove(shoppingCart);
+                        }
+                        await _db.SaveChangesAsync();
+                    }
+                    else
+                    {
+                        cartItemInCart.Quantity = newQuantity;
+                        await _db.SaveChangesAsync();
+                    }
+                }
+            }
+            return response;
+
+        }
+    }
+}
