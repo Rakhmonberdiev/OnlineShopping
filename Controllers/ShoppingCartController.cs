f7e4a99101ce164db943800618b148f25da1588d
Jamoliddin <j.m.raxmonberdiyev@gmail.com>
2023-09-17 15:03:03 +0500

    Get Shopping Cart

--- Controllers/ShoppingCartController.cs
+++ Controllers/ShoppingCartController.cs
@@ -21,6 +21,40 @@ namespace OnlineShopping.Controllers
         }
 
 
+        [HttpGet]
+        public async Task<ActionResult<ApiResponse>> GetShoppingCart(string userId)
+        {
+            try
+            {
+                ShoppingCart shoppingCart;
+                if (string.IsNullOrEmpty(userId))
+                {
+                    shoppingCart = new();
+                }
+                else
+                {
+                    shoppingCart = await _db.ShoppingCarts
+                    .Include(u => u.CartItems).ThenInclude(u => u.Product)
+                    .FirstOrDefaultAsync(u => u.UserId == userId);
+
+                }
+                if (shoppingCart.CartItems != null && shoppingCart.CartItems.Count > 0)
+                {
+                    shoppingCart.CartTotal = shoppingCart.CartItems.Sum(u => u.Quantity * u.Product.Price);
+                }
+                response.Result = shoppingCart;
+                response.StatusCode = HttpStatusCode.OK;
+                return Ok(response);
+            }
+            catch (Exception ex)
+            {
+                response.IsSuccess = false;
+                response.ErrorMessages
+                     = new List<string>() { ex.ToString() };
+                response.StatusCode = HttpStatusCode.BadRequest;
+            }
+            return response;
+        }
 
         [HttpPost]
         public async Task<ActionResult<ApiResponse>> AddOrUpdateItemInCart(string userId, int productId, int updateQuantityBy)
