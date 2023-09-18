2e06c12bbab55cd86a577f491953ef48739f0443
Jamoliddin <j.m.raxmonberdiyev@gmail.com>
2023-09-18 20:11:07 +0500

    Get Order

---
+++ Controllers/OrderController.cs
@@ -0,0 +1,82 @@
+﻿using Azure;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using OnlineShopping.Data;
+using OnlineShopping.Models;
+using System.Net;
+
+namespace OnlineShopping.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrderController : ControllerBase
+    {
+        private readonly AppDbContext _db;
+        private ApiResponse _rs;
+        public OrderController(AppDbContext appDbContext)
+        {
+            _db = appDbContext;
+            _rs = new ApiResponse();
+        }
+        [HttpGet]
+        public async Task<ActionResult<ApiResponse>> GetOrders(string userId)
+        {
+            try
+            {
+                var orderHeaders = _db.OrderHeaders.Include(u=>u.OrderDetails).ThenInclude(u=>u.Product).OrderByDescending(u=>u.OrderHeaderId);
+                if(!string.IsNullOrEmpty(userId))
+                {
+                    _rs.Result = orderHeaders.Where(u=>u.ApplicationUserId == userId);
+                }
+                else
+                {
+                    _rs.Result = orderHeaders;
+                }
+                _rs.StatusCode = HttpStatusCode.OK;
+                return Ok(_rs);
+            }
+            catch (Exception ex)
+            {
+                _rs.IsSuccess = false;
+                _rs.ErrorMessages = new List<string>() { ex.ToString() };
+            }
+            return _rs;
+        }
+
+        [HttpGet("{id:int}")]
+        public async Task<ActionResult<ApiResponse>> GetOrders(int id)
+        {
+            try
+            {
+                if (id == 0)
+                {
+                    _rs.StatusCode = HttpStatusCode.BadRequest;
+                    return BadRequest(_rs);
+                }
+
+
+                var orderHeaders = _db.OrderHeaders.Include(u => u.OrderDetails)
+                    .ThenInclude(u => u.Product)
+                    .Where(u => u.OrderHeaderId == id);
+                if (orderHeaders == null)
+                {
+                    _rs.StatusCode = HttpStatusCode.NotFound;
+                    return NotFound(_rs);
+                }
+                _rs.Result = orderHeaders;
+                _rs.StatusCode = HttpStatusCode.OK;
+                return Ok(_rs);
+            }
+            catch (Exception ex)
+            {
+                _rs.IsSuccess = false;
+                _rs.ErrorMessages
+                     = new List<string>() { ex.ToString() };
+            }
+            return _rs;
+        }
+
+
+    }
+}
