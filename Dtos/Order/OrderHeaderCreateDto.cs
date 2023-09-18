f83bc31dcf19cafd7a69ab4763439a92dd4b9d30
Jamoliddin <j.m.raxmonberdiyev@gmail.com>
2023-09-18 17:15:11 +0500

    Order DTOs

---
+++ Dtos/Order/OrderHeaderCreateDto.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OnlineShopping.Dtos.Order
+{
+    public class OrderHeaderCreateDto
+    {
+        [Required]
+        public string PickupName { get; set; }
+        [Required]
+        public string PickupPhoneNumber { get; set; }
+        [Required]
+        public string PickupEmail { get; set; }
+
+        public string ApplicationUserId { get; set; }
+        public double OrderTotal { get; set; }
+
+
+        public string StripePaymentIntentID { get; set; }
+        public string Status { get; set; }
+        public int TotalItems { get; set; }
+
+        public IEnumerable<OrderDetailsCreateDto> OrderDetailsDto { get; set; }
+    }
+}
